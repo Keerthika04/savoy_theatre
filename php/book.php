eb18b36be62c1b9c28a62367641c56db94dd323d
Keerthika04 <jkeerthi144@gmail.com>
2024-06-04 20:03:29 +0530

    Feat: Booking Function

--- php/book.php
+++ php/book.php
@@ -8,8 +8,7 @@ if (!isset($_SESSION['user_id'])) {
     $_SESSION['booking'] = true;
 }
 
-$name = $_SESSION['username']; 
-$mobile = $_SESSION['phone_no']; 
+$name = $_SESSION['username'];
 $movie = $_POST['movie'];
 $date = $_POST['date'];
 $time = $_POST['time'];
@@ -19,19 +18,49 @@ $children = $_POST['children'];
 $parking = $_POST['parking'];
 $totalPrice = $_POST['totalPrice'];
 
-// Generate booking ID
-$query = "SELECT MAX(CAST(SUBSTRING(booking_id, 2, LENGTH(booking_id)-1) AS UNSIGNED)) AS maxID FROM booking";
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_assoc($result);
-$maxID = $row['maxID'];
-$newID = 'b' . str_pad($maxID + 1, 4, '0', STR_PAD_LEFT);
+if ($parking !== "none") {
+    $query = $db->query("SELECT parking_id FROM parking ORDER BY parking_id DESC LIMIT 1");
+    $newParkingID = "p0001";
 
-$query = "INSERT INTO booking (booking_id, customer_id, showtime_id, date, adult, children, total, parking_id, seats) VALUES ('$newID', '$_SESSION[customer_id]', '$movie', '$date', '$time', '$seats', '$adults', '$children', '$parking', '$totalPrice')";
-if (mysqli_query($conn, $query)) {
-    echo "Booking successful!";
-} else {
-    echo "Error: " . mysqli_error($conn);
+    if ($query->num_rows > 0) {
+        $row = $query->fetch_assoc();
+        $last_id = $row['parking_id'];
+        $num = (int) substr($last_id, 1) + 1;
+        $newParkingID = "p" . str_pad($num, 4, "0", STR_PAD_LEFT);
+    }
+
+    $query = "INSERT INTO parking (parking_id, vehicle) VALUES (?,?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ss", $newParkingID, $parking);
+    $stmt->execute();
 }
 
-mysqli_close($conn);
+$query = $db->query("SELECT booking_id FROM booking ORDER BY booking_id DESC LIMIT 1");
+$newID = "b0001";
+
+if ($query->num_rows > 0) {
+    $row = $query->fetch_assoc();
+    $last_id = $row['booking_id'];
+    $num = (int) substr($last_id, 1) + 1;
+    $newID = "b" . str_pad($num, 4, "0", STR_PAD_LEFT);
+}
+
+$query = "SELECT show_id FROM showtimes WHERE date =? AND time =?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("ss", $date, $time);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$showID = $row['show_id'];
+
+$query = "INSERT INTO booking (booking_id, customer_id, showtime_id, date, adult, children, total, seats, parking_id) VALUES (?,?,?,?,?,?,?,?,?)";
+$stmt = $db->prepare($query);
+$stmt->bind_param("sssssssss", $newID, $_SESSION['user_id'], $showID, $date, $adults, $children, $totalPrice, $seats, $newParkingID);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo "Booking successful!";
+} else {
+    echo "Error: " . $db->error;
+}
 ?>
