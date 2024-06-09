a70d53c38a710fe4313374db70ea41a2860604e6
Keerthika04 <jkeerthi144@gmail.com>
2024-06-09 19:27:33 +0530

    Feat: Index and movies details changes

--- index.php
+++ index.php
@@ -12,6 +12,26 @@ if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
   }
 }
 
+$query = $db->query("SELECT movie_id, movie_title, movie_card_poster, duration, released_date FROM movies where 	current_movies = 1");
+
+$currentMovies = array();
+
+if ($query->num_rows > 0) {
+  while ($row = $query->fetch_assoc()) {
+    $currentMovies[] = $row;
+  }
+}
+
+$query = $db->query("SELECT 	movie_id, 	movie_title, movie_card_poster, duration, released_date FROM movies where 	upcoming_movies = 1");
+
+$upcomingMovies = array();
+
+if ($query->num_rows > 0) {
+    while ($row = $query->fetch_assoc()) {
+        $upcomingMovies[] = $row;
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +43,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
   <title>Savoy</title>
   <link rel="icon" href="./Images/favicon.png" type="image/png" />
   <link rel="stylesheet" href="css/styles.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" /> -->
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
@@ -67,25 +87,202 @@ if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
         if ($query->num_rows > 0) {
           while ($row = $query->fetch_assoc()) {
             $imageURL = 'uploaded_banner_images/' . $row["banner_poster"];
-            ?>
+        ?>
             <div class="swiper-slide">
               <img src="<?php echo $imageURL; ?>" alt="" />
               <div class="movie_detail">
                 <h1><?php echo htmlspecialchars($row["movie_title"]); ?></h1>
                 <a href="php/booking.php?movie=<?php echo htmlspecialchars($row["movie_id"]); ?>">Buy Tickets</a>
-                <a href="php/movie_details.php?movie=<?php echo htmlspecialchars($row["movie_id"]); ?>"
-                  class="border_btn">More Info</a>
+                <a href="php/movie_details.php?movie=<?php echo htmlspecialchars($row["movie_id"]); ?>" class="border_btn">More Info</a>
               </div>
             </div>
-          <?php }
+        <?php }
         } ?>
       </div>
 
       <div class="swiper-pagination"></div>
     </div>
   </section>
+
+  <section class="movies_slider">
+    <div class="container">
+      <div class="blurry-bg"></div>
+      <div class="black_overlay"></div>
+      <div class="swiper available tranding-slider contents">
+        <h1>Now Playing Movies</h1>
+        <div class="swiper-wrapper">
+          <?php foreach ($currentMovies as $movie) : ?>
+            <?php $imageURL = 'uploaded_card_images/' . $movie["movie_card_poster"]; ?>
+            <div class="tranding-slide movies_slider swiper-slide" data-movie-id="<?= htmlspecialchars($movie["movie_id"]); ?>">
+              <img src="<?= $imageURL ?>" alt="<?= $movie['movie_title'] ?>">
+              <div class="movie-info">
+                <h2><?= $movie['movie_title'] ?></h2>
+                <p><?= $movie['duration'] ?></p>
+                <p>Release on <?= $movie['released_date'] ?></p>
+                <a href="php/booking.php?movie=<?php echo htmlspecialchars($movie["movie_id"]); ?>" class="buy-tickets"> <span></span><span></span><span></span>
+                  <span></span>Buy Tickets</a>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="upcoming_movie_slider">
+    <div class="container">
+      <div class="blurry-bg2"></div>
+      <div class="black_overlay"></div>
+      <div class="swiper upcoming_movies tranding-slider contents">
+        <h1>Upcoming Movies</h1>
+        <div class="swiper-wrapper">
+          <?php foreach ($upcomingMovies as $movie) : ?>
+            <?php $imageURL = 'uploaded_card_images/' . $movie["movie_card_poster"]; ?>
+            <div class="tranding-slide movies_slider swiper-slide" data-movie-id="<?= htmlspecialchars($movie["movie_id"]); ?>">
+              <img src="<?= $imageURL ?>" alt="<?= $movie['movie_title'] ?>" class="upcomingImg">
+              <div class="movie-info">
+                <h2><?= $movie['movie_title'] ?></h2>
+                <p><?= $movie['duration'] ?></p>
+                <p>Release on <?= $movie['released_date'] ?></p>
+                <a href="php/booking.php?movie=<?php echo htmlspecialchars($movie["movie_id"]); ?>" class="buy-tickets"> <span></span><span></span><span></span>
+                  <span></span>Buy Tickets</a>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <div class="links">
+        <div class="column">
+            <h2>Our Company</h2>
+            <ul>
+                <li>Our Brands</li>
+                <li>Contact Us</li>
+                <li>FAQs</li>
+                <li>Corporate Information</li>
+                <li>Savoy Investor Connect</li>
+                <li>Investor Relations</li>
+                <li>Media Center</li>
+                <li>Careers</li>
+                <li>Savoy Privacy Policy</li>
+                <li>Terms & Conditions</li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Movies</h2>
+            <ul>
+                <li>Movies</li>
+                <li>Theatres</li>
+                <li>Ratings Information</li>
+                <li>IMAX at Savoy</li>
+                <li>Dolby Cinema at Savoy</li>
+                <li>PRIME at Savoy</li>
+                <li>RealD 3D</li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Programming</h2>
+            <ul>
+                <li>Private Theatre Rentals</li>
+                <li>Savoy Artisan Films</li>
+                <li>Savoy Thrills & Chills</li>
+                <li>Savoy Screen Unseen</li>
+                <li>Fan Faves</li>
+                <li>International Films</li>
+                <li>Film Festivals</li>
+                <li>Special Events</li>
+                <li>Sensory Friendly Films</li>
+                <li>Groups & Events</li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>More</h2>
+            <ul>
+                <li>Offers & Promotions</li>
+                <li>Gift Cards</li>
+                <li>Movie Merchandise</li>
+                <li>Savoy Merchandise</li>
+                <li>NFTs from Savoy</li>
+                <li>Mobile App</li>
+                <li>Savoy Scene</li>
+                <li>Request Refund</li>
+            </ul>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="logo">SAVOY THEATRES</div>
+        <p>WE MAKE BEST MOVIE EXPERIENCE.</p>
+        <p>&copy; Copyright 2024 Savoy Theatres</p>
+    </footer>
+
 </body>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script src="js/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    let TrandingSlider = new Swiper('.available', {
+      effect: 'coverflow',
+      centeredSlides: true,
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 30,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1.5,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChange: function() {
+          let activeImage = this.slides[this.activeIndex].querySelector('img');
+          let backgroundImage = 'url(' + activeImage.src + ')';
+          document.querySelector('.blurry-bg').style.backgroundImage = backgroundImage;
+        },
+      },
+    });
+    let upcomingSlider = new Swiper('.upcoming_movies', {
+      effect: 'coverflow',
+      centeredSlides: true,
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 30,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChange: function() {
 
+          let upcomingImg = this.slides[this.activeIndex].querySelector('.upcomingImg');
+          let backgroundImage = 'url(' + upcomingImg.src + ')';
+          document.querySelector('.blurry-bg2').style.backgroundImage = backgroundImage;
+        },
+      },
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.tranding-slide').forEach(function(slide) {
+    slide.addEventListener('click', function(event) {
+      if (!event.target.closest('.buy-tickets')) {
+        var movieId = this.getAttribute('data-movie-id');
+        window.location.href = 'php/movie_details.php?movie=' + movieId;
+      }
+    });
+  });
+});
+</script>
+<script src="js/script.js"></script>
 </html>
--- php/movie_details.php
+++ php/movie_details.php
@@ -3,13 +3,13 @@ session_start();
 require 'db_connection.php';
 
 if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
-  if (isset($_SESSION['user_id'])) {
-    header("Location: user_profile.php");
-    exit();
-  } else {
-    header("Location: login.php");
-    exit();
-  }
+    if (isset($_SESSION['user_id'])) {
+        header("Location: user_profile.php");
+        exit();
+    } else {
+        header("Location: login.php");
+        exit();
+    }
 }
 
 ?>
@@ -22,7 +22,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
     <title>Savoy</title>
     <link rel="icon" href="../Images/favicon.png" type="image/png" />
     <link rel="stylesheet" href="../css/styles.css" />
-    <link rel="stylesheet" href="../css/movie_detail.css" />
+    <link rel="stylesheet" href="../css/movie_detailssss.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
@@ -67,70 +67,74 @@ if (isset($_GET['action']) && $_GET['action'] == 'check_session') {
         if ($query->num_rows > 0) {
             while ($row = $query->fetch_assoc()) {
                 $imageURL = '../uploaded_movie_posters/' . $row["movie_poster"];
-                ?>
+        ?>
                 <div class="nav_movie" id="nav_movie">
                     <div class="left">
-                    <h2><?php echo htmlspecialchars($row["movie_title"]); ?></h2>
-                    <h4><span>Duration :</span> <?php echo htmlspecialchars($row["duration"]); ?></h4>
+                        <h2><?php echo htmlspecialchars($row["movie_title"]); ?></h2>
+                        <h4><span>Duration :</span> <?php echo htmlspecialchars($row["duration"]); ?></h4>
                     </div>
                     <div class="right">
-                    <a href="booking.php?movie=<?php echo htmlspecialchars($row["movie_id"]);?>">Buy Tickets</a>
+                        <a href="booking.php?movie=<?php echo htmlspecialchars($row["movie_id"]); ?>">Buy Tickets</a>
                     </div>
                 </div>
-            </nav>
+    </nav>
 
-            <section class="banner">
-                <div class="swiper-container">
-                    <div class="swiper-wrapper">
+    <section class="banner">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide movie_poster">
+                    <div class="black_overlay"></div>
+                    <img src="<?php echo $imageURL; ?>" alt="" />
+                    <div class="movie_detail">
+                        <h1 class="with_language"><?php echo htmlspecialchars($row["movie_title"]); ?></h1>
+                        <h3>Language - <?php echo htmlspecialchars($row["language"]); ?></h3>
 
-                        <div class="swiper-slide">
-                            <img src="<?php echo $imageURL; ?>" alt="" />
-                            <div class="movie_detail">
-                                <h1 class="with_language"><?php echo htmlspecialchars($row["movie_title"]); ?></h1>
-                                <h3>Language - <?php echo htmlspecialchars($row["language"]); ?></h3>
-                                <a href="booking.php?movie=<?php echo htmlspecialchars($row["movie_id"]);?>">Buy Tickets</a>
-                                <a href="<?php echo htmlspecialchars($row["movie_trailer"]); ?>" class="border_btn">Watch
-                                    Trailer</a>
-                            </div>
-                        </div>
+                        <?php if ($row["upcoming_movies"] == 0) : ?>
+                            <a href="booking.php?movie=<?php echo htmlspecialchars($row["movie_id"]); ?>">Buy Tickets</a>
+                        <?php endif; ?>
 
+                        <a href="<?php echo htmlspecialchars($row["movie_trailer"]); ?>" class="border_btn">Watch
+                            Trailer</a>
                     </div>
                 </div>
-            </section>
-            <section class="movie_details">
-                <div class="about_movie">
-                    <div class="left">
-                        <h1><span>Duration :</span> <?php echo htmlspecialchars($row["duration"]); ?></h1>
-                        <h1><span>Released on :</span> <?php echo htmlspecialchars($row["released_date"]); ?></h1>
-                        <h1><?php echo htmlspecialchars($row["genre"]); ?></h1>
-                    </div>
-                    <div class="right">
-                        <p><?php echo htmlspecialchars($row["storyplot"]); ?></p>
-                    </div>
-                </div>
-                <div class="about_cast">
-                    <div class="left">
-                        <h1>Cast & Crew</h1>
-                    </div>
-                    <div class="right">
-                        <?php
-                        $sqlData = $row["movie_cast"];
-                        $sqlData = preg_replace('/\s*,\s*/', ',', trim($sqlData));
-                        $items = explode(',', $sqlData);
 
-                        foreach ($items as $item) {
-                            preg_match('/(.*?)(\s*\[.*\])/', $item, $matches);
-                            $name = $matches[1];
-                            $role = $matches[2];
+            </div>
+        </div>
+    </section>
+    <section class="movie_details">
+        <div class="about_movie">
+            <div class="left">
+                <h1><span>Duration :</span> <?php echo htmlspecialchars($row["duration"]); ?></h1>
+                <h1><span>Released on :</span> <?php echo htmlspecialchars($row["released_date"]); ?></h1>
+                <h1><?php echo htmlspecialchars($row["genre"]); ?></h1>
+            </div>
+            <div class="right">
+                <p><?php echo htmlspecialchars($row["storyplot"]); ?></p>
+            </div>
+        </div>
+        <div class="about_cast">
+            <div class="left">
+                <h1>Cast & Crew</h1>
+            </div>
+            <div class="right">
+                <?php
+                $sqlData = $row["movie_cast"];
+                $sqlData = preg_replace('/\s*,\s*/', ',', trim($sqlData));
+                $items = explode(',', $sqlData);
 
-                            echo "<h1>" . htmlspecialchars($name) . "<span> &nbsp" . htmlspecialchars($role) . "</span></h1>";
-                        }
-                        ?>
-                    </div>
-                </div>
-            </section>
+                foreach ($items as $item) {
+                    preg_match('/(.*?)(\s*\[.*\])/', $item, $matches);
+                    $name = $matches[1];
+                    $role = $matches[2];
 
-        <?php }
+                    echo "<h1>" . htmlspecialchars($name) . "<span> &nbsp" . htmlspecialchars($role) . "</span></h1>";
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+<?php }
         } ?>
 
 </body>
